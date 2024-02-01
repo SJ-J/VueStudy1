0246cd312592ed679deeccaa94bc86d253dbd4e0
SJ-J <92574014+SJ-J@users.noreply.github.com>
2024-02-01 20:39:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<template>
+
+<div class="modal">
+  <div v-for="(detail, i) in rooms" :key="i">
+    <div class="black-bg" v-if="modalOpen == true && roomId === detail.id">
+      <div class="white-bg">
+        <h4>{{ detail.title }}</h4>
+        <div>
+          {{ detail.content }}
+        </div>
+        <button @click="closePop">닫기</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div class="menu">
+    <a v-for="(menu, i) in menuName" :key="i"> {{ menu }} </a> 
+    <span id="userHi" v-if="1 == 2"> 박무현 님 안녕하세요! </span>
+    <span id="userHi" v-else-if="1 == 3"> 박무현5 님 안녕하세요! </span>
+    <span id="userHi" v-else> 로그인 후 이용해주세요. </span>
+  </div>
+
+  <div class="shop">
+    <div v-for="(shop, i) in rooms" :key="i">
+      <img :src="shop.image" class="roomImg">
+      <h4 @click="openPop(i)"> {{ shop.title }} </h4>
+      <p> {{ shop.price }} 원</p>
+      <!-- <button @click="increase(i)">허위매물신고</button> <span>신고 수 : {{ reportCnt[i] }} 건</span> -->
+    </div>
+  </div>
+  
+</template>
+
+<script>
+
+import roomInfo from './assets/oneroom.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {  // object 형식{k:v}으로 저장
+      index : 0,
+      reportCnt : [0, 0, 0, 0, 0, 0],
+      menuName : ['Home', 'Shop', 'About'],
+      // products : ['백호동 원룸', '현무동 원룸', '주작동 원룸', '청룡동 원룸'],
+      rooms : roomInfo,
+      // 단순 String값으로 처리되기 때문에, require를 통해 import 해주면 엑박 없이 이미지 잘 뜸!!
+      // imageUrl : [require('./assets/room0.jpg'), require('./assets/room1.jpg'), require('./assets/room2.jpg'), require('./assets/room3.jpg')],
+      modalOpen : false,
+      roomId : null,
+    }
+  },
+  methods : { // 함수는 이 자리 고정!!
+    increase(index) {
+      for (let i = 0; i < 1; i++) {
+        this.reportCnt[index]++;
+        console.log(index);
+      }
+    },
+    openPop(roomId) {
+      for (let i = 0; i < 1; i++) {
+        this.modalOpen = true;
+        this.roomId = roomId;
+        console.log(roomId);
+      }
+    },
+    closePop() {
+      this.modalOpen = false;
+    }
+  },
+  components: {
+  }
+}
+
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: rgb(67, 92, 124);
+  padding: 15px;
+  border-radius: 5px;
+  width: 100%;
+  position: fixed;
+}
+
+.menu a {
+  color: aliceblue;
+  padding: 10px;
+}
+
+#userHi {
+  color: aliceblue;
+  padding: 10px;
+}
+
+.roomImg {
+  width: 400px;
+  height: 250px;
+  margin-top: 40px;
+}
+
+</style>
---
+++ src/assets/oneroom.js
@@ -0,0 +1,43 @@
+export default [{
+    id : 0,
+    title: "백호동 20호",
+    image: "https://codingapple1.github.io/vue/room0.jpg",
+    content: "예쁜 꽃🥀이 자라고 있어요. 공기청정기 제공",
+    price: 340000
+    },
+    {
+    id : 1,
+    title: "백호동 22호",
+    image: "https://codingapple1.github.io/vue/room1.jpg",
+    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+    price: 450000
+    },
+    {
+    id : 2,
+    title: "백호동 23호",
+    image: "https://codingapple1.github.io/vue/room2.jpg",
+    content: "내부 리모델링을 마친 방입니다. 책상 위를 조심하세요!",
+    price: 780000
+    },
+    {
+    id : 3,
+    title: "백호동 26호",
+    image: "https://codingapple1.github.io/vue/room3.jpg",
+    content: "깔끔한 입주청소가 완료된 방입니다. 아주 청결해요🌟",
+    price: 550000
+    },
+    {
+    id : 4,
+    title: "백호동 28호",
+    image: "https://codingapple1.github.io/vue/room4.jpg",
+    content: "온갖 간식이 팬트리를 꽉꽉 채우고 있어요. 무료 옵션!",
+    price: 680000
+    },
+    {
+    id : 5,
+    title: "백호동 38호",
+    image: "https://codingapple1.github.io/vue/room5.jpg",
+    content: "치실, 워터픽, 가글 완비! 가끔 물 새는거 빼면 좋아요💦",
+    price: 370000
+  }];
+
